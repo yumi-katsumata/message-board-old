d8861df3c4b31610b3dc7be0b8bcebf5569a716f
sakaihi <sakaihi@gmail.com>
2016-04-15 07:23:32 +0000

    add destroy

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,5 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
     @message = Message.new
@@ -28,6 +28,11 @@ class MessagesController < ApplicationController
     else
       render 'edit'
     end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
   private
